3d91a1b893fd5fe8f9507f29aaa400cf979dd8d9
thomas.crambert <thomas.crambert@epita.fr>
2021-08-17 16:59:07 +0200

    dunno

--- lisp/cb-custom.el
+++ lisp/cb-custom.el
@@ -3,4 +3,10 @@
   (load-file "~/.emacs.d/init.el")
   )
 
+(defun cb/add-hooks (hook-list)
+  "perform add-hook on every elements of the list"
+  (mapcar (lambda (hook-list)
+            (funcall 'add-hook hook-list 'company-mode) hook-list)
+          hook-list))
+
 (provide 'cb-custom)
